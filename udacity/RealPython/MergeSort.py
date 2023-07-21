7f86660f21dd54050969d07e0c894e525988d3a0
Edwin Mesa <edwinsar.mes@gmail.com>
2023-07-20 23:19:25 -0500

    new version python

--- udacity/RealPython/MergeSort.py
+++ udacity/RealPython/MergeSort.py
@@ -1,3 +1,25 @@
+from random import randint
+from timeit import repeat
+
+def run_sorting_algorithm(algorithm, array):
+    # Set up the context and prepare the call to the specified
+    # algorithm using the supplied array. Only import the
+    # algorithm function if it's not the built-in `sorted()`.
+    setup_code = f"from __main__ import {algorithm}" \
+        if algorithm != "sorted" else ""
+
+    stmt = f"{algorithm}({array})"
+
+    # Execute the code ten different times and return the time
+    # in seconds that each execution took
+    times = repeat(setup=setup_code, stmt=stmt, repeat=3, number=10)
+
+    # Finally, display the name of the algorithm and the
+    # minimum time it took to run
+    print(f"Algorithm: {algorithm}. Minimum execution time: {min(times)}")
+
+ARRAY_LENGTH = 10000
+
 def merge(left, right):
     # If the first array is empty, then nothing needs
     # to be merged, and you can return the second array as the result
@@ -7,6 +29,9 @@ def merge(left, right):
     # to be merged, and you can return the first array as the result
     if len(right) == 0:
         return left    
+    
+
+
     result     = []
     index_left = index_right = 0
     # Now go through both arrays until all the elements
@@ -53,5 +78,14 @@ def merge_sort(array):
     )
 
 
-mg = merge_sort([3,2,5,6,8,9,1])
-print(mg)
+# mg = merge_sort([3,2,5,6,8,9,1])
+# print(mg)
+
+if __name__ == "__main__":
+    # Generate an array of `ARRAY_LENGTH` items consisting
+    # of random integer values between 0 and 999
+    array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+
+    # Call the function using the name of the sorting algorithm
+    # and the array you just created
+    run_sorting_algorithm(algorithm="merge_sort", array=array)
